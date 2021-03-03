2428237298f96178f05cf2e944b01a5e91436b71
Piotr Czajkowski <pczajkowski@gmail.com>
2021-03-03 10:51:04 +0100

    DRY

--- extractText.c
+++ extractText.c
@@ -28,19 +28,17 @@ int main(int argc, char **argv) {
 	if (strcmp(fi->ext, "docx") == 0) {
 		Stopif(!transformLoad((char*)gDocxToTxtData, gDocxToTxtSize), return 1, "Can't load transform!\n");
 		readZIP(fi->fullname, wordFile);
-		transformCleanup();
 	}
 	else if (strcmp(fi->ext, "pptx") == 0) {
 		Stopif(!transformLoad((char*)gPptxToTxtData, gPptxToTxtSize), return 1, "Can't load transform!\n");
 		readZIP(fi->fullname, slideFiles);
 		readZIP(fi->fullname, drawingFiles);
-		transformCleanup();
 	}
 	else if (strcmp(fi->ext, "xlsx") == 0) {
 		Stopif(!transformLoad((char*)gXlsxToTxtData, gXlsxToTxtSize), return 1, "Can't load transform!\n");
 		readZIP(fi->fullname, xlsxFile);
-		transformCleanup();
 	}
 
+	transformCleanup();
 	freeFileInfo(fi);
 }
